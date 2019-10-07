0a20ca37f7f74e07ae7c83f12dfe76943ff726d7
Mircea Lungu <mircea.lungu@gmail.com>
2019-10-07 14:05:36 +0200

    translate

--- translate.py
+++ translate.py
@@ -1,11 +1,13 @@
 import ast
 import inspect
-from fibonacci import fibonacci
+from fibonacci import fibonacci, lucas
 
 
 class JavascriptTranslator(ast.NodeVisitor):
     """
+
         Translate a python function into Javascript
+
     """
 
     def __init__(self):
@@ -16,23 +18,73 @@ class JavascriptTranslator(ast.NodeVisitor):
         self.visit(node)
         return self.src
 
+    def visit_FunctionDef(self, defn):
+        arglist = ",".join(name.arg
+                           for name in
+                           defn.args.args)
+        js_defn = f"var {defn.name} = function ({arglist})"
+        js_defn += "\n{\n"
+
+        self.src += self._indent(js_defn)
+
+        self.indent += 4
+        for stmt in defn.body:
+            self.visit(stmt)
+        self.indent -= 4
+
+        self.src += self._indent("}\n")
+
+    def visit_If(self, if_):
+        self.src += self._indent("if (")
+        self.visit(if_.test)
+        self.src += ") {\n"
+
+        self.indent += 4
+        for stmt in if_.body:
+            self.visit(stmt)
+        self.indent -= 4
+
+        self.src += self._indent("}\n")
+
+    def visit_Compare(self, compare):
+        self.visit(compare.left)
+        self.src += " "
+        self.visit(compare.ops[0])
+        self.src += " "
+        self.visit(compare.comparators[0])
+
+    def visit_Name(self, name):
+        self.src += name.id
+
+    def visit_Eq(self, eq):
+        self.src += " == "
+
+    def visit_Num(self, num):
+        self.src += str(num.n)
+
+    def visit_Add(self, add):
+        self.src += "+"
+
+    def visit_Call(self, call):
+        self.visit(call.func)
+
+    def visit_BinOp(self, binop):
+        self.visit(binop.left)
+        self.visit(binop.op)
+        self.visit(binop.right)
+
+    def visit_Return(self, retu):
+        self.src += self._indent("return ")
+        if retu.value:
+            self.src += " "
+            self.visit(retu.value)
+        self.src += ";\n"
+
     def _indent(self, code):
         return (" " * self.indent) + code
 
-    def visit_FunctionDef(self, fundef):
-        arg_list = ",".join(name.arg for name in fundef.args.args)
-        js_definition = f"var {fundef.name} = function ({arg_list})"
-        js_definition += "{\n"
-        self.src += js_definition
 
-        # ...
-
-        self.src += "}\n"
-
-
-
-
-def convert_to_js(func):
+def js(func):
     func_source = inspect.getsource(func)
     func_ast = ast.parse(func_source)
 
@@ -40,4 +92,4 @@ def convert_to_js(func):
     return translator.translate(func_ast)
 
 
-print(convert_to_js(fibonacci))
+print(js(fibonacci))
