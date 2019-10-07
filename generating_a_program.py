be9d6c7bc222a48a0bf20400e5dce98ed72868b3
Mircea Lungu <mircea.lungu@gmail.com>
2019-10-07 13:16:06 +0200

    worked in class

--- generating_a_program.py
+++ generating_a_program.py
@@ -1,6 +1,4 @@
 import ast
-from astkit.render import SourceCodeRenderer
-
 from copy import copy
 
 # Program being generated
@@ -12,40 +10,51 @@ from copy import copy
 #     x = (x + 1)
 # print(x)
 
-use_x = ast.Name(id="x", ctx=ast.Load())
-assign_x = ast.Name(id="x", ctx=ast.Store())
+# reusable variables
+use_x = ast.Name(id="x",
+                 ctx=ast.Load())
+x_lhs = ast.Name(id="x",
+                 ctx=ast.Store())
 one = ast.Num(n=1)
 two = ast.Num(n=2)
 four = ast.Num(n=4)
 
-# x=2
-initial_assign = ast.Assign(targets=[copy(assign_x)], value=copy(two))
+# initial assign
+initial_assign = ast.Assign(
+    targets=[copy(x_lhs)],
+    value=copy(two))
+
 initial_assign.lineno = 1
 initial_assign.col_offset = 0
-ast.fix_missing_locations(initial_assign)  # applies lineno and col_offset recursively to the subtree... not
+ast.fix_missing_locations(initial_assign)
+
+# print statement
+print_statement = ast.Expr(
+    value=ast.Call(
+        func=ast.Name(id="print", ctx=ast.Load()),
+        args=[use_x],
+        keywords=[]
+    )
+)
+print_statement.lineno = 2
+print_statement.col_offset = 0
+ast.fix_missing_locations(print_statement)
 
 
-# the if conditional
+
 if_ = ast.If()
 if_.test = ast.Compare()
 if_.test.left = copy(use_x)
 if_.test.ops = [ast.Eq()]
 if_.test.comparators = [copy(four)]
 
-# print(x)
-print_x = ast.Expr(
-    value=ast.Call(
-        func=ast.Name(id="print", ctx=ast.Load()),
-        args=[copy(use_x)],
-        keywords=[]))
-
-if_.body = [print_x]
+if_.body = [print_statement]
 
 x_plus_1 = ast.BinOp(left=copy(use_x),
                      op=ast.Add(),
                      right=copy(one))
 
-assign_increment = ast.Assign(targets=[copy(assign_x)],
+assign_increment = ast.Assign(targets=[copy(x_lhs)],
                               value=x_plus_1)
 
 if_.orelse = [assign_increment]
@@ -54,31 +63,15 @@ if_.lineno = 2
 if_.col_offset = 0
 ast.fix_missing_locations(if_)
 
+program = ast.Module(
+    body=[
+        initial_assign,
+        if_,
+        print_statement
+    ]
+)
 
-# print(x)
-final_print = ast.Expr(
-    value=ast.Call(
-        func=ast.Name(id="print", ctx=ast.Load()),
-        args=[copy(use_x)],
-        keywords=[]))
-
-final_print.lineno = 6
-final_print.col_offset = 0
-ast.fix_missing_locations(final_print)
-
-# put everything together
-mod = ast.Module(body=[
-    initial_assign,
-    if_,
-    final_print])
-
-code = compile(mod, '', 'exec')
-print(type(code))
-
+code = compile(program, '', 'exec')
 exec(code)
-print(" ")
 
-print(ast.dump(mod, include_attributes=True))
-print(" ")
-
-print(SourceCodeRenderer.render(mod))
+print(ast.dump(if_))
