64ecb7e4d3bfa2a0d59b26feab4f8a14d5e4894f
Mircea Lungu <mircea.lungu@gmail.com>
2019-10-07 19:59:32 +0200

    Update dump.py

--- dump.py
+++ dump.py
@@ -1,5 +1,12 @@
 import ast
 
+"""
+    To explore the ast for a given 
+    construct first parse the AST from
+    a string version of the code; then
+    dump the AST object. 
+"""
+
 p = ast.parse("def fun():"
               "    pass")
 print(ast.dump(p))
